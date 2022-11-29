b6da3461d501a7eb53604e6ba958acb1309a0f48
Daniel Amorim <dan-amorim@live.com>
2022-11-29 09:10:46 -0300

    Melhorando usabilidade dos botões e suas funções

--- AgendaWPFeEntityFramework/MainWindow.xaml.cs
+++ AgendaWPFeEntityFramework/MainWindow.xaml.cs
@@ -33,11 +33,76 @@ namespace AgendaWPFeEntityFramework
             contato.email = txtEmail.Text;
             contato.telefone = txtTelefone.Text;
 
-            using (agendaEntities ctx = new agendaEntities())
+            if(operacao == "inserir")
             {
-                ctx.contatos.Add(contato);
-                ctx.SaveChanges();
+                using (agendaEntities ctx = new agendaEntities())
+                {
+                    ctx.contatos.Add(contato);
+                    ctx.SaveChanges();
+                }
             }
+
+            listarContatos();
+            alterarBotoes(1);
+            limparCampos();
+
+        }
+
+        private void btInserir_Click(object sender, RoutedEventArgs e)
+        {
+            operacao = "inserir";
+            alterarBotoes(2);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listarContatos();
+            alterarBotoes(1);
+            txtID.Text = "";
+            txtID.IsEnabled = false;
+        }
+
+        private void listarContatos()
+        {
+            agendaEntities ctx = new agendaEntities();
+            var consulta = ctx.contatos;
+            dgDados.ItemsSource = consulta.ToList();
+        }
+        
+        private void alterarBotoes (int opcao)
+        {
+            btAlterar.IsEnabled = false;
+            btInserir.IsEnabled = false;
+            btExcluir.IsEnabled = false;
+            btCancelar.IsEnabled = false;
+            btLocalizar.IsEnabled = false;
+            btSalvar.IsEnabled = false;
+
+            if (opcao == 1)
+            {
+                btInserir.IsEnabled = true;
+                btLocalizar.IsEnabled = true;
+            }
+            if (opcao == 2)
+            {
+                btSalvar.IsEnabled = true;
+                btCancelar.IsEnabled = true;
+            }
+        }
+
+        private void limparCampos()
+        {
+            txtID.IsEnabled = true;
+            txtID.Clear();
+            txtEmail.Clear();
+            txtNome.Clear();
+            txtTelefone.Clear();
+        }
+
+        private void btCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            alterarBotoes(1);
+            limparCampos();
         }
     }
 }
